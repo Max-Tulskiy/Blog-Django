d0e7fafa1697e4634eb402dbc32fc959bfdb9c57
Max Tulskiy <maks.tulskiy.04@gmail.com>
2023-07-20 14:03:00 +0300

    fixed new features, added new bugs

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,24 +1,26 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset()\
-            .filter(status=Post.Status.PUBLISHED)
-
+            .filter(status=Post.Status.PUBLISHED) # super не полностью переопределяет метод объекта
 class Post(models.Model):
 
     objects = models.Manager() #менеджер по умолчанию
     published = PublishedManager() #конкретно-прикладной менеджер
+
+
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length = 250)
-    slug = models.SlugField(max_length = 250)
+    slug = models.SlugField(max_length = 250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE, #поле указывает, что при удалении пользователя, бд удалит все его посты
                                related_name='blog_posts')
@@ -38,3 +40,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # динамическое формирование url адреса
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,18 +1,61 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404, HttpResponse
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger  # Постраничная разбивка
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
-def post_detail(request, id):
+# Create your views here.
+'''
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+'''
+
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
-                  {'post':post})
+                  {'post': post})
+
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    # Постраничная разбивка
+    paginator = Paginator(post_list, 3)  # 3 поста на страницу
+    page_number = request.GET.get('page', 1)  # получаем номер страницы, если нет, то возвращаем 1
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(request,
                   'blog/post/list.html',
-                  {'posts':posts})
+                  {'posts': posts})
+
+
+def post_share(request, post_id):
+    # Извлекаем пост по id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        # передача формы на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+        return render(request,
+                      'blog/post/share.html',
+                      {'post': post, 'form': form})

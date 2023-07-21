ae4a226b9cb625972f336110f555464b87b64cff
Max Tulskiy <maks.tulskiy.04@gmail.com>
2023-07-22 01:24:27 +0300

    added sending posts by e-mail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,13 +6,13 @@ from .forms import EmailPostForm
 from django.core.mail import send_mail
 
 # Create your views here.
-'''
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
-'''
+
 
 
 def post_detail(request, year, month, day, post):
@@ -27,6 +27,7 @@ def post_detail(request, year, month, day, post):
                   {'post': post})
 
 
+'''
 def post_list(request):
     post_list = Post.published.all()
 
@@ -44,6 +45,7 @@ def post_list(request):
                   'blog/post/list.html',
                   {'posts': posts})
 
+'''
 
 def post_share(request, post_id):
     # Извлекаем пост по id
@@ -58,16 +60,15 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url = request.build_absolute_url(post.get_absolute_url())
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
             subject = f"{cd['name']} recommends you read " \
                       f"{post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
-                      f"{cd['name']}'s comments: {cd['comments']}"
-            send_mail(subject, message, 'maks.tulskiy.04@gmail.com',
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
                       [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
-        return render(request,
-                      'blog/post/share.html',
-                      {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})

dade2fedb9691e1955a73785f2b3a69f60f69fc4
Max Tulskiy <maks.tulskiy.04@gmail.com>
2023-07-22 01:21:18 +0300

    added sending posts by e-mail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger  # Постраничная разбивка
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 '''
@@ -49,11 +50,22 @@ def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
+
+    sent = False
+
     if request.method == 'POST':
         # передача формы на обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'maks.tulskiy.04@gmail.com',
+                      [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
         return render(request,
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -111,3 +111,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'maks.tulskiy.04@gmail.com'
+EMAIL_HOST_PASSWORD = 'pfbjhbsmhliccria'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
